b6557fed75f7ea7a99ff57a87fc51e86c379678d
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-10-01 13:41:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(function(){
-  
+  $(document).ready(function(){
+    $("#save").click(function(){
+      var cash = $("#cash").val();
+      var res = $("#red").val();
+      var comm = $("#comm").val();
+      var ind = $("#ind").val();
+      $.cookie('cash', cash, { expires : 7 });
+      $.cookie('res', res, { expires : 7 });
+      $.cookie('comm', comm, { expires : 7 });
+      $.cookie('ind', ind, { expires : 7 });
+      });
+      checkCookieValues();
+  });
+  function checkCookieValues(){
+    if ($.cookie('cash')!=="undefined") {
+      $("#cash").html($.cookie('cash'));
+      $("#res").html($.cookie('res'));
+      $("#comm").html($.cookie('comm'));
+      $("#ind").html($.cookie('ind'));
+    }
+  }
 });
